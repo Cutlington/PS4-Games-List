b545813e224d7a7b441433128532ed5f39965149
Cutlington <jcutlet23@gmail.com>
2026-03-30 15:00:15 +1100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -31,7 +31,7 @@ const genreGroups = {
     "Horror": "Horror",
     "Indie": "Indie",
     "VR": "VR",
-    "Survival": "Survival"
+    "Survival": "Survival",
 };
 
 // ------------------------------
